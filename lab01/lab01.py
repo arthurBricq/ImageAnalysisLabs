f0bbbedd573f4d7f3ede679a2ff7d793637bc14b
arthurbricq <arthur.bricq@epfl.ch>
2020-03-29 19:44:03 +0200

    Working on Fourier Descriptors. Adding the invariance study file

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -9,9 +9,14 @@ import matplotlib.pyplot as plt
 import lab01_functions as imPro
 import skimage.io
 import skimage.morphology
-from skimage.color import rgb2gray
 import numpy as np
-import YonasModule.morphological_edge_detection as IP_med
+from skimage.segmentation import active_contour
+from skimage.filters import gaussian
+from skimage.segmentation import chan_vese
+from skimage.feature import canny
+from scipy import ndimage
+from skimage.color import rgb2gray
+from skimage import exposure
 
 
 #%% PART I *******************************
@@ -27,13 +32,13 @@ ax.set_title('MRI brain image ({} px, {} px)'.format(im_h, im_w))
 ax.axis('off')
 plt.show()
 
+
 #%% Functions to encapsulate the 2 edge detectors that were done
 
 # Those 2 functions return an edge map with negative pixels where the edges are
 # Those edge maps are then used in a labelization algortimh to either
 # - count the pixels (part I)
 # - cluster the pixels in groups and find the number of different shapes
-
 
 def getEdgeMapMethod1(img, t1, t2):
     binary_im = np.logical_and(img > t1, brain_im < t2) 
@@ -48,22 +53,19 @@ def getEdgeMapMethod2(img, t1, t2):
     (edges,thld_img,G,Phi,G_thld_img)=imPro.skeletonize_based_sobel(img,sigma,t1,t2,G_t1,G_t2)
     edgeMap = -np.array(edges).astype(int)
     return edgeMap
-    
 
-#%% Edge detection 1 (arthur) or 2 (Jonas)
-    
-# Best thresholds for arthur: 40 to 100
-# Best thresholds for jonas: 55 to 105  (?)
-
-# Example on how to use the edge detection function 
-edgeMap = getEdgeMapMethod2(brain_im,40,100)
-imPro.plotEdgeMap(edgeMap)
+def getEdgeMapMethod3(img):
+    edgeMap = canny(img, sigma= 1.1)
+    edgeMap = skimage.morphology.dilation(edgeMap)
+    edgeMap = -edgeMap.astype(int)
+    imPro.plotEdgeMap(edgeMap)
+    return edgeMap
+ 
 
 #%% labelization 
 
 # edgeMap = getEdgeMapMethod1(brain_im,40,100)
-edgeMap = getEdgeMapMethod2(brain_im,55,105)
-imPro.plotEdgeMap(edgeMap)
+edgeMap = 3*getEdgeMapMethod3(brain_im)
 
 # Get the labels to count the number of pixels
 labels = imPro.labelizePixels(edgeMap)
@@ -90,13 +92,11 @@ im2 = skimage.io.imread('data/lab-01-data/arena-shapes-02.png')
 im3 = skimage.io.imread('data/lab-01-data/arena-shapes-03.png')
 images = np.array([im1,im2,im3])
 
-
 #%% 
-
 im = im2
 imPro.plotImage(im)
 
-
+#%%
 # 1. Seperate the background from the shapes with optimal threshold 
 tmp = im[:,:,0]
 imPro.plotImage(tmp)
@@ -123,6 +123,211 @@ print("COULEUR BLEUE")
 groups_blue = imPro.countNumberOfShapes(im_bi_blue)
 print("COULEUR NOIRE")
 groups_black = imPro.countNumberOfShapes(im_bi_black)
+
+#%% To count the number of pixels on each image
+
+
+for i,im in enumerate(images):
+    # 1. Seperate the background from the shapes with optimal threshold on the red channel
+    tmp = im[:,:,0]
+    t = skimage.filters.threshold_otsu(tmp)
+    im_bi = tmp < t 
+
+    # 2. Obtain the black shapes
+    tmp = im[:,:,2]
+    tmp = skimage.filters.gaussian(tmp,sigma = 2.5) * 255
+    tmp = skimage.exposure.rescale_intensity(tmp,out_range = (0,255))
+    im_bi_black = imPro.blueThresholdForPixel((358,56),tmp)
+    
+    # 3. Obtain the blue shapes
+    im_bi_blue = np.logical_xor(im_bi,im_bi_black)
+    im_bi_blue = skimage.morphology.opening(im_bi_blue)
+
+    # Count the number of pixels on each color
+    n_blue = np.count_nonzero(im_bi_blue)
+    n_black = np.count_nonzero(im_bi_black)
+    
+    print("Image :",i+1)
+    print("Color blue: {} pixels".format(n_blue))
+    print("Color black: {} pixels".format(n_black))
+    print("\n")
+    
+
+    
+#%% data processing function
+    
+
+    
+#%% Picture by picture
+im1 = skimage.io.imread('data/lab-01-data/arena-shapes-01.png')
+im2 = skimage.io.imread('data/lab-01-data/arena-shapes-02.png')
+im3 = skimage.io.imread('data/lab-01-data/arena-shapes-03.png')    
+
+im = im2
+
+# 1. Seperate the background from the shapes with optimal threshold on the red channel
+tmp = im[:,:,0]
+t = skimage.filters.threshold_otsu(tmp)
+im_bi = tmp < t 
+imPro.plotImage(im_bi)
+
+# Get the edge map (to be used later) and create
+edgeMap = skimage.filters.laplace(im_bi)
+edgeMap[edgeMap>0]=-edgeMap[edgeMap>0]
+imPro.plotEdgeMap(edgeMap)
+labels = imPro.labelizePixels(edgeMap)
+
+# 2. Obtain the black shapes
+tmp = im[:,:,2]
+tmp = skimage.filters.gaussian(tmp,sigma = 2.5) * 255
+imPro.plotHistogram(tmp)
+tmp = skimage.exposure.rescale_intensity(tmp,out_range = (0,255))
+imPro.plotHistogram(tmp)
+im_bi_black = imPro.blueThresholdForPixel((358,56),tmp)
+
+# 3. Obtain the blue shapes
+im_bi_blue = np.logical_xor(im_bi,im_bi_black)
+im_bi_blue = skimage.morphology.opening(im_bi_blue)
+
+# 5. Find the centers of each cluster.
+centers_blue = findCentersOfClusters(im_bi_blue)
+centers_black = findCentersOfClusters(im_bi_black)
+
+
+# 6. From the clusters of each color, find the associated cluster in the edge map
+# and count the number of pixels within those clusters.
+pixels_blue = 0
+for center in centers_blue.astype(int):
+    label = labels[center[0],center[1]]
+    count = labels[labels==label].shape[0]
+    pixels_blue += count
+    print(count)
+print("BLEU {}".format(pixels_blue))
+
+pixels_black = 0
+for center in centers_black.astype(int):
+    label = labels[center[0],center[1]]
+    count = labels[labels==label].shape[0]
+    pixels_black += count
+    print(count)
+print("BLACK {}".format(pixels_black))
+
+# Count the number of pixels on each color
+n_blue = np.count_nonzero(im_bi_blue)
+n_black = np.count_nonzero(im_bi_black)
+
+print("Color blue: {} pixels".format(n_blue))
+print("Color black: {} pixels".format(n_black))
+print("\n")
+
+
+
+
+
+#%%
+
+tmp = im1[:,:,0]
+t = skimage.filters.threshold_otsu(tmp)
+im_bi = tmp < t 
+imPro.plotImage(im_bi)
+edgeMap = skimage.filters.laplace(im_bi)
+imPro.plotEdgeMap(edgeMap)
+
+
+
+#%% Just trying some things
+
+# Grayscale conv: black = 0 and white = 255
+
+im = im3
+im = rgb2gray(im)
+background = im > 0.4
+im[background] = 0.0
+im = gaussian(im,sigma=0)
+hist,bins = exposure.histogram(im)
+im_bi = np.logical_and(im > 30.0 / 255, im < 70.0 / 255)
+
+fig, axs = plt.subplots(1,3,figsize=(10,4))
+axs[0].plot(hist[1:])
+axs[1].imshow(im, cmap = 'gray')
+axs[2].imshow(im_bi,cmap='gray')
+
+#%%
+
+# Grayscale conv: black = 0 and white = 255
+from scipy.signal import savgol_filter
+from scipy.signal import find_peaks
+
+im = im3
+im = rgb2gray(im)
+t = skimage.filters.threshold_otsu(im)
+background = im > t 
+im[background] = 0.0
+im = gaussian(im,sigma=0)
+hist,bins = exposure.histogram(im)
+# the hist always has 2 peaks
+
+array = savgol_filter(hist[1:],61,2)
+peaks = find_peaks(array,height=50,distance=10)[0]
+
+t1 = peaks[0] / 2 / 255
+t2 = (peaks[1]-peaks[0]) / 255
+im_bi = np.logical_and(im > t1 , im < t2)
+
+
+fig, axs = plt.subplots(1,3,figsize=(10,4))
+axs[0].plot(array)
+axs[1].imshow(im > t1, cmap = 'gray')
+axs[2].imshow(im_bi,cmap='gray')
+
+
+#%% 
+
+im_eq = exposure.equalize_hist(im)
+imPro.plotImage(im_eq)
+
+im_bi1 = im_eq > 0.04
+im_bi2 = im < 0.4
+
+fig, axs = plt.subplots(1,2,figsize=(10,4))
+axs[0].imshow(im_bi2 > 0.04,cmap='gray')
+axs[1].imshow(im_bi2,cmap='gray')
+
+fig1, axs = plt.subplots(1,3,figsize=(10,4))
+axs[0].imshow(im,cmap='gray')
+axs[1].hist(im.ravel(), bins=256)
+axs[2].hist(im_eq.ravel(), bins=256)
+axs[1].set_yticklabels([])
+axs[2].set_yticklabels([])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ lab02/arthur/fourier_invariance.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 15:59:03 2020
+
+@author: abric
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+sys.path.append("..")
+
+import lab02_functions as imPro
+import skimage
+import skimage.transform
+
+# import matplotlib
+# matplotlib.use('Qt5Agg')
+
+# %% Get the image and the contour
+
+zeros = imPro.get_zeros()
+ones = imPro.get_ones()
+
+
+# %%
+def plot_fourier_descriptors(fourier):
+    fix, axs = plt.subplots(2, 1, figsize=(5, 5))
+    axs[0].plot(np.abs(fourier))
+    axs[1].plot(np.angle(fourier))
+
+
+# %% Normalize routine
+# https://dsp.stackexchange.com/questions/19982/fourier-descriptors-trying-to-classify-objects
+
+transform = skimage.transform.SimilarityTransform(scale=0.9)
+img1 = ones[1]
+img2 = skimage.transform.warp(img, transform)
+
+# Then compute the fourier transform
+[X1, Y1] = imPro.get_outmost_contour(img1)
+[X2, Y2] = imPro.get_outmost_contour(img2)
+fourier1 = np.fft.fft(X1 + 1j * Y1)
+fourier2 = np.fft.fft(X2 + 1j * Y2)
+# fourier1 = normalize_fourier(fourier1)
+# fourier2 = normalize_fourier(fourier2)
+fourier1 = np.fft.fftshift(fourier1)
+fourier2 = np.fft.fftshift(fourier2)
+
+plot_fourier_descriptors(fourier1)
+plot_fourier_descriptors(fourier2)
+plt.show()
+
+# Plot the 2 images just to see if it is correct
+fig, axs = plt.subplots(1, 2, figsize=(5, 5))
+axs[0].imshow(img1)
+axs[1].imshow(img2)
+plt.show()
+
+# %% Rotation matrix (the modulus is invarient anyway)
+theta = np.pi / 3
+R = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+
+img1 = ones[1]
+X1, Y1 = imPro.get_outmost_contour(img1)
+contour1 = np.column_stack([X1, Y1])
+contour2 = contour1 @ R
+X2, Y2 = contour2[:, 0], contour2[:, 1]
+
+signal1 = X1 + 1j * Y1
+fourier1 = np.fft.fft(signal1)
+
+signal2 = X2 + 1j * Y2
+fourier2 = np.fft.fft(signal2)
+
+fig, ax = plt.subplots()
+ax.plot(np.angle(fourier1), '-.r', label = 'contour')
+ax.plot(np.angle(fourier2),'-.b', label = 'rotated contour')
+ax.set_title('Effect of rotation over the Fourier Descriptors phase')
+ax.legend()
+plt.show()
+
+# %% Scaling operation (the phase is invariant anyway)
+R = np.array([[1.2, 0],
+              [0, 1.2]])
+
+img1 = ones[1]
+X1, Y1 = imPro.get_outmost_contour(img1)
+contour1 = np.column_stack([X1, Y1])
+contour2 = contour1 @ R
+X2, Y2 = contour2[:, 0], contour2[:, 1]
+
+signal1 = X1 + 1j * Y1
+fourier1 = np.fft.fft(signal1)
+
+signal2 = X2 + 1j * Y2
+fourier2 = np.fft.fft(signal2)
+
+fig, ax = plt.subplots()
+ax.plot(np.abs(fourier1)[1:5], '-.r', label = 'Contour')
+ax.plot(np.abs(fourier2)[1:5],'-.b', label = 'Scaled contour')
+ax.set_title('Effect of scaling over the Fourier Descriptors amplitude')
+ax.set_xlabel('First harmonics')
+ax.legend()
+plt.show()
+
+
