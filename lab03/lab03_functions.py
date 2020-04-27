034bf87337daa3fa17b5e7a785997d4a62e7ef03
Johan <johan.felisaz@prontmail.com>
2020-04-27 14:25:57 +0200

    Adding lab03 to git with initial function set

---
+++ lab03/lab03_functions.py
@@ -0,0 +1,29 @@
+
+import os
+import scipy.io
+
+
+
+
+def get_matlab_data():
+    """
+    Returns the matlab data as numpy array for the lab 3
+
+    Returns
+    -------
+    a : 200x2 array
+        Data array for the lab.
+    b : 200x2 array
+        Data array for the lab.
+    c : 200x2 array
+        Data array for the lab.
+        
+    """
+    data_folder = 'lab-03-data'
+    data_part1_path = os.path.join(data_folder, 'part1', 'classification.mat')
+    matfile = scipy.io.loadmat(data_part1_path)
+    a = matfile['a']
+    b = matfile['b']
+    c = matfile['c']
+    return a,b,c
+
